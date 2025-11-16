11020b5752f8555be12db9fc0299666ac663c71c
ritik <ritik.m@quantique.ai>
2025-11-16 11:42:09 +0530

    multiple function handeling in single route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const app = express();
 
-// app.get("/abc", (req, res) => {
-//   res.send("hello from abcccccccc");
+app.get(
+  "/abc",
+  [
+    (req, res, next) => {
+      // res.send("hello from abc function 1");
+      next();
+    },
+    (req, res, next) => {
+      // res.send("hello from abc function 2");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    res.send("hello from abc function 3");
+    next();
+  },
+  (req, res, next) => {
+    res.send("hello from abc function 4");
+    next();
+  }
+);
+// app.post("/user", (req, res) => {
+//   res.send("user created");
 // });
-app.post("/user", (req, res) => {
-  res.send("user created");
-});
-app.get("/user/:userId/:name/:age", (req, res) => {
-  console.log(req.params);
-  res.send({ name: "Ritik", age: "30" });
-});
-app.delete("/user/delete", (req, res) => {
-  res.send("user deleted");
-});
+// app.get("/user/:userId/:name/:age", (req, res) => {
+//   console.log(req.params);
+//   res.send({ name: "Ritik", age: "30" });
+// });
+// app.delete("/user/delete", (req, res) => {
+//   res.send("user deleted");
+// });
 
 // app.use("/", (req, res) => {
 //   res.send("hello from dashboard");
